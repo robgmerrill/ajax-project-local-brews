beae8d1bb465cddb77c02de2837fb1f30e644af9
Rob Merrill <robgmerrill@gmail.com>
2023-02-24 12:09:39 -0800

    finish feature one

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var dataObject = {
+    visited: [],
+    view: '',
+    nextEntryId: 1,
+    editing: null
+}
+
+const previousBreweriesJSON = localStorage.getItem('breweries-local-storage')
+
+if (previousBreweriesJSON !== null) {
+    dataObject.visited = JSON.parse(previousBreweriesJSON);
+  }
+
+window.addEventListener('beforeunload', function (event) {
+    const breweriesJSON = JSON.stringify(dataObject.visited);
+    localStorage.setItem('breweries-local-storage', breweriesJSON);
+  });
